6d439b75d41c8d6a77ba6b61de50a6cf4996cdae
bgocean <bigocean700@gmail.com>
2023-12-25 14:18:01 +0200

    modal

---
+++ script/index.js
@@ -0,0 +1,25 @@
+const modal = document.querySelector(".cart-modal__overlay");
+const cartBtn = document.querySelector("#cart-button");
+
+const openModal = () => {
+  modal.classList.add("open");
+};
+
+const closeModal = () => {
+  modal.classList.remove("open");
+};
+
+cartBtn.addEventListener("click", () => {
+  openModal();
+});
+
+modal.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("cart-modal__overlay") ||
+    e.target.closest(".cart-modal__header--close")
+  ) {
+    closeModal();
+  }
+});
+
+// lesson11 28.00
