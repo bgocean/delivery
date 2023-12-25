e8f54b7a8ce9debbbef42fe03d9fc89ea1772870
bgocean <bigocean700@gmail.com>
2023-12-25 20:26:56 +0200

    add js

--- script/index.js
+++ script/index.js
@@ -1,25 +1,138 @@
-const modal = document.querySelector(".cart-modal__overlay");
-const cartBtn = document.querySelector("#cart-button");
+const modalFunc = () => {
+  const modal = document.querySelector(".cart-modal__overlay");
+  const cartBtn = document.querySelector("#cart-button");
 
-const openModal = () => {
-  modal.classList.add("open");
+  const openModal = () => {
+    modal.classList.add("open");
+  };
+
+  const closeModal = () => {
+    modal.classList.remove("open");
+  };
+
+  cartBtn.addEventListener("click", () => {
+    openModal();
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("cart-modal__overlay") ||
+      e.target.closest(".cart-modal__header--close")
+    ) {
+      closeModal();
+    }
+  });
 };
 
-const closeModal = () => {
-  modal.classList.remove("open");
-};
+const restsFunc = () => {
+  const container = document.querySelector("#rests-container");
 
-cartBtn.addEventListener("click", () => {
-  openModal();
-});
+  const restArray = [
+    {
+      id: 0,
+      title: "Пицца плюс",
+      time: 50,
+      raiting: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "pizza-plus.jpg",
+    },
+    {
+      id: 1,
+      title: "Тануки",
+      time: 50,
+      raiting: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "tanuki.jpg",
+    },
+    {
+      id: 2,
+      title: "FoodBand",
+      time: 50,
+      raiting: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "foodband.jpg",
+    },
+    {
+      id: 3,
+      title: "Жадина-пицца",
+      time: 50,
+      raiting: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "zhadina.jpg",
+    },
+    {
+      id: 4,
+      title: "Точка еды",
+      time: 50,
+      raiting: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "tochka.jpg",
+    },
+    {
+      id: 5,
+      title: "PizzaBurger",
+      time: 50,
+      raiting: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "pizzaburger.jpg",
+    }
+  ];
 
-modal.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("cart-modal__overlay") ||
-    e.target.closest(".cart-modal__header--close")
-  ) {
-    closeModal();
+  const loading = () => {
+    container.innerHTML =
+      '<p style="width: 100%; text-align: center;">Загрузка...</p>';
+  };
+
+  const renderRests = (array) => {
+    container.innerHTML = "";
+    array.forEach((card) => {
+      container.insertAdjacentHTML(
+        "beforeend",
+        `
+            <a href="./goods.html?id=${card.id}" class="products-card">
+                <div class="products-card__image">
+                  <img src="./images/rests/${card.image}" alt="${card.image}">
+                </div>
+                <div class="products-card__description">
+                  <div class="products-card__description-row">
+                    <h4 class="products-card__description--title">${card.title}</h4>
+                    <div class="products-card__description--badge">${card.time} мин</div>
+                  </div>                  
+                  <div class="products-card__description-row">
+                    <div class="products-card__description-info">
+                      <div class="products-card__description-info--raiting">
+                        <img src="./images/icons/star.svg" alt="star">
+                        ${card.raiting}
+                      </div>
+                      <div class="products-card__description-info--price">
+                        От ${card.price} ₽
+                      </div>
+                      <div class="products-card__description-info--group">
+                        ${card.type}
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </a>
+      `
+      );
+    });
+  };
+
+  if (container) {
+    loading();
+
+    setTimeout(() => {
+      renderRests(restArray);
+    }, 1000);
   }
-});
+};
 
-// lesson11 28.00
+modalFunc();
+restsFunc();
